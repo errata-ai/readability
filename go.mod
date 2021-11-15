dcd57621c0bc044eb7c667c3d1289b003e76f6c5
Joseph Kato <joseph@jdkato.io>
2021-11-14 16:23:41 -0800

    New test layout

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/errata-ai/readability
+
+go 1.17
+
+require github.com/google/go-cmdtest v0.3.0
+
+require (
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/google/renameio v0.1.0 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/google/go-cmdtest"
+)
+
+var update = flag.Bool("update", false, "replace test file contents with output")
+
+func Test(t *testing.T) {
+	ts, err := cmdtest.Read("testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Setup = func(_ string) error {
+		_, testFileName, _, ok := runtime.Caller(0)
+		if !ok {
+			return fmt.Errorf("failed get real working directory from caller")
+		}
+
+		projectRootDir := filepath.Join(filepath.Dir(testFileName), "testdata")
+		if err := os.Setenv("ROOTDIR", projectRootDir); err != nil {
+			return fmt.Errorf("failed change 'ROOTDIR' to caller working directory: %v", err)
+		}
+
+		return nil
+	}
+
+	path, err := exec.LookPath("vale")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Commands["vale"] = cmdtest.Program(path)
+	ts.Commands["cdf"] = cmdtest.InProcessProgram("cdf", cdf)
+
+	ts.Run(t, *update)
+}
